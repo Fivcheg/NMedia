b4bef3742904aa63bfa4cc242396f4387f0b6d97
Vitalii Novgorodov <vital00011@rambler.ru>
2022-12-11 16:53:57 +0300

    Project move to AndroidX

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,6 +1,6 @@
 package ru.netology.nmedia
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
