ceae397a0ce48220a4131eebe8fbeecc6db22e99
Vitalii Novgorodov <vital00011@rambler.ru>
2022-12-30 17:54:13 +0300

    RecyclerView posts.map

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
 import kotlin.math.roundToInt
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,35 +16,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        subscribe()
-        setupListener()
-    }
 
-    private fun subscribe() {
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                author.text = post.author
-                published.text = post.published
-                content.text = post.content
-                likedCount.text = countTransform(post.likes)
-                sharedCount.text = countTransform(post.shares)
-                viewsCount.text = countTransform(post.views)
-                liked.setImageResource(if (post.likedByMe) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24)
-                if (post.sharedByMe) shared.setImageResource(R.drawable.ic_baseline_share_ok_24)
+        viewModel.data.observe(this) { posts ->
+            binding.container.removeAllViews()
+            posts.map { post ->
+                CardPostBinding.inflate(layoutInflater, binding.container, true).apply {
+                    author.text = post.author
+                    published.text = post.published
+                    content.text = post.content
+                    likedCount.text = countTransform(post.likes)
+                    sharedCount.text = countTransform(post.shares)
+                    viewsCount.text = countTransform(post.views)
+                    liked.setImageResource(
+                        if (post.likedByMe) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24
+                    )
+                    liked.setOnClickListener {
+                        viewModel.likeById(post.id)
+                    }
+                    shared.setImageResource(
+                        if (post.sharedByMe) R.drawable.ic_baseline_share_ok_24 else R.drawable.ic_baseline_share_24
+                    )
+                    shared.setOnClickListener {
+                        viewModel.shareById(post.id)
+                    }
+                }.root
             }
         }
-    }
-
-    private fun setupListener() {
-
-        binding.liked.setOnClickListener {
-            viewModel.like()
-        }
-
-        binding.shared.setOnClickListener {
-            viewModel.share()
-        }
-
     }
 
     private fun countTransform(x: Int): String {
@@ -72,6 +69,10 @@ class MainActivity : AppCompatActivity() {
         return lengthNumber
     }
 }
+
+
+
+
 
 
 
